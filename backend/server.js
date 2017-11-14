9b8044304df4d1bfd65ba2e1a42a6262eb20b116
Ramiro Gonzalez <ramiro.gonzalez@vonpix.com>
2017-11-14 18:12:43 +0100

    Adding concurrently to run APP + Server at the same time. Removing file saving.

--- backend/server.js
+++ backend/server.js
@@ -12,15 +12,10 @@ app.post('/upload', function(req, res) {
         return res.status(400).send('No files were uploaded.');
 
     // The name of the input field (i.e. "file") is used to retrieve the uploaded file
-    let sampleFile = req.files.file;
+    var theFile = req.files.file;
+    
+    // TODO - Do something with the file
 
-    // Use the mv() method to place the file somewhere on your server
-    var _path = `uploads/${parseInt(Math.random() * 10000)}_${sampleFile.name}`;
-    sampleFile.mv(_path, function(err) {
-        if (err)
-            return res.status(500).send(err);
-        res.send('File uploaded!');
-    });
 });
 
 app.listen(port);
