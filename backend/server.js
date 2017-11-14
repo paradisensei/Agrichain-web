09b745a31e9fe9bd992afb56d2e7d14ebf11e17f
Ramiro Gonzalez <ramiro.gonzalez@vonpix.com>
2017-11-14 18:17:57 +0100

    Fixing file indentation

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,20 @@
 var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3001;
+  app = express(),
+  port = process.env.PORT || 3001;
 
 const fileUpload = require('express-fileupload');
 
 // default options
 app.use(fileUpload());
 
-app.post('/upload', function(req, res) {
-    if (!req.files)
-        return res.status(400).send('No files were uploaded.');
+app.post('/upload', function (req, res) {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
 
-    // The name of the input field (i.e. "file") is used to retrieve the uploaded file
-    var theFile = req.files.file;
-    
-    // TODO - Do something with the file
+  // The name of the input field (i.e. "file") is used to retrieve the uploaded file
+  var theFile = req.files.file;
+
+  // TODO - Do something with the file
 
 });
 
